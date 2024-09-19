64aae6061a5af4cc7b258a76886cf5bd662b4a42
Ezequiel <ezequielleonzybert@gmail.com>
2024-09-19 00:53:41 -0300

    done

--- index.js
+++ index.js
@@ -14,26 +14,37 @@
 const puppeteer = require('puppeteer');
 const email = 'norapo2550@sigmazon.com';
 const password = '123456';
+let url;
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: true, // No muestra la interfaz gráfica
+        headless: false, // No muestra la interfaz gráfica
         args: ['--no-sandbox', '--disable-setuid-sandbox'] // Configuración para entornos remotos
     });
     const page = await browser.newPage();
-    await page.goto('https://www.correoargentino.com.ar/MiCorreo/public/');
-    await page.type('#email', email);
-    await page.type('#password', password);
-    await page.keyboard.press('Enter');
-    await page.type('#email', email);
-    await page.type('#password', password);
-    await page.keyboard.press('Enter');
+    url = 'https://www.correoargentino.com.ar/MiCorreo/public/';
+    await page.goto(url);
+    while (url == page.url()) {
+        await page.type('#email', email);
+        await page.type('#password', password);
+        await page.keyboard.press('Enter');
+        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    }
 
-    await page.waitForSelector('h3');
-    const h3Text = await page.evaluate(() => {
-        return document.querySelector('h3').innerText;
+    await page.goto('https://www.correoargentino.com.ar/MiCorreo/public/envioCla');
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    await page.type('#largo', 40);
+    await page.type('#ancho', 40);
+    await page.type('#alto', 5);
+    await page.type('#peso', 1);
+    await page.type('#valorContenido', 1);
+    await page.click('#next');
+
+    await page.waitForSelector('#mediL');
+    const mediL = await page.evaluate(() => {
+        return document.querySelector('#mediL').innerText;
     });
-    console.log('Contenido del <h3>:', h3Text);
+    console.log('Contenido del mediL:', mediL);
 
     await browser.close();
 })();
