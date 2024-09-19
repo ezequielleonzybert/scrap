c42c1ab83d2b64fe5e3881406863a53998fd29d0
Ezequiel <ezequielleonzybert@gmail.com>
2024-09-19 00:53:52 -0300

    donedone

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-// const port = process.env.PORT || 4000;
-
-// import { createServer } from 'node:http';
-
-// const server = createServer((req, res) => {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end('Hello World!\n');
-// });
-
-// server.listen(port, '0.0.0.0', () => {
-//     console.log(`Listening on 0.0.0.0:${port}`);
-// });
-
 const puppeteer = require('puppeteer');
 const email = 'norapo2550@sigmazon.com';
 const password = '123456';
 let url;
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false, // No muestra la interfaz gráfica
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Configuración para entornos remotos
-    });
+    const browser = await puppeteer.launch({ headless: 'shell' });
     const page = await browser.newPage();
+    console.log("login in");
     url = 'https://www.correoargentino.com.ar/MiCorreo/public/';
     await page.goto(url);
     while (url == page.url()) {
         await page.type('#email', email);
         await page.type('#password', password);
         await page.keyboard.press('Enter');
-        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        await page.waitForNavigation();
+    }
+    console.log("logged");
+    url = 'https://www.correoargentino.com.ar/MiCorreo/public/envioCla';
+    console.log("setting box properties");
+    await page.goto(url);
+    await page.waitForSelector('#largo');
+    await page.type('#largo', '40');
+    await page.waitForSelector('#ancho');
+    await page.type('#ancho', '40');
+    await page.waitForSelector('#alto');
+    await page.type('#alto', '5');
+    await page.waitForSelector('#peso');
+    await page.type('#peso', '1');
+    await page.waitForSelector('#valorContenido');
+    await page.type('#valorContenido', '1');
+    await page.waitForSelector('#next');
+    await page.click('#next');
+    console.log("setting delivery type");
+    await page.waitForSelector('#tipoEntrega');
+    await page.select('#tipoEntrega', 'sucursal');
+    await page.waitForSelector('#provincia2');
+    await page.type('#provincia2', 'BUENOS AIRES');
+    await page.waitForSelector('#sucursalDestino2');
+    await page.click('#sucursalDestino2');
+    await page.type('#sucursalDestino2', 'HAEDO');
+    await page.keyboard.press('Enter');
+
+    console.log("getting price");
+    await page.waitForSelector('#CLASICO');
+    let precio = '';
+    while (!precio.includes('$')) {
+        precio = await page.evaluate(() => {
+            return document.querySelector('#CLASICO').innerText;
+        });
     }
 
-    await page.goto('https://www.correoargentino.com.ar/MiCorreo/public/envioCla');
-    await page.waitForNavigation({ waitUntil: 'networkidle2' });
-    await page.type('#largo', 40);
-    await page.type('#ancho', 40);
-    await page.type('#alto', 5);
-    await page.type('#peso', 1);
-    await page.type('#valorContenido', 1);
-    await page.click('#next');
-
-    await page.waitForSelector('#mediL');
-    const mediL = await page.evaluate(() => {
-        return document.querySelector('#mediL').innerText;
-    });
-    console.log('Contenido del mediL:', mediL);
-
+    console.log('Precio:', precio);
+    url = 'https://www.correoargentino.com.ar/MiCorreo/public/logout';
+    await page.goto(url);
     await browser.close();
 })();
